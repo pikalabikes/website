56077a23a201490dc3b25d2b1ca15b202386c537
jakubfiala <j4kub.fiala@gmail.com>
2024-01-25 19:25:45 +0100

    some initial styling and stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
-import react from "@astrojs/react";
-
 const dev = import.meta.env.MODE === 'development';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pikalabikes.github.io',
   base: dev ? '/website' : '',
-  integrations: [mdx(), react()]
+  integrations: [mdx()]
 });
--- src/components/section.jsx
+++
@@ -1,6 +0,0 @@
-export default ({ id, title, children }) => (
-  <section id={id}>
-    <h2>{title}</h2>
-    {children}
-  </section>
-);
